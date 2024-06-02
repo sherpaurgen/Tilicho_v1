ffb5823ac5382db0f76bc7f19aa9b1fff48c2ecf
urgen sherpa <sherpaurgen@users.noreply.github.com>
2024-06-03 00:36:59 +0545

    feature: Add maxBytesError to check body size to restrict to 1MB

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -42,29 +42,42 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dest interface{}) error {
 	//dest destination
-	err := json.NewDecoder(r.Body).Decode(dest)
+	const maxBytes = 1048576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	//err := json.NewDecoder(r.Body).Decode(dest)
+	dcoder := json.NewDecoder(r.Body)
+	dcoder.DisallowUnknownFields() //allows decoder to return error when input doesnt match target struct
+	err := dcoder.Decode(dest)
 	if err != nil {
+		fmt.Println(err)
 		// while reading json from reqest body there is likely to happen errors below
 		var syntaxError *json.SyntaxError
 		var unmarshallTypeError *json.UnmarshalTypeError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
 		switch {
 		case errors.As(err, &syntaxError):
-			return fmt.Errorf("bad json body at character %d", syntaxError.Offset)
+			return fmt.Errorf("readJSON: bad json body invalid character %d", syntaxError.Offset)
 		case errors.Is(err, io.ErrUnexpectedEOF):
-			return errors.New("body contains badly formed json")
+			return errors.New("readJSON: body contains badly formed json")
 		case errors.As(err, &unmarshallTypeError):
 			if unmarshallTypeError.Field != "" {
-				return fmt.Errorf("body contains incorrect json type %q", unmarshallTypeError.Field)
+				return fmt.Errorf("readJSON: body contains incorrect json type for field %q", unmarshallTypeError.Field)
 			}
-			return fmt.Errorf("body contains incorrect %d", unmarshallTypeError.Offset)
+			return fmt.Errorf("readJSON: body contains incorrect type at character %d", unmarshallTypeError.Offset)
 		case errors.Is(err, io.EOF):
-			return errors.New("body must not be empty")
+			return errors.New("readJSON: body must not be empty")
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("readJSON: body must not be larger than 1MB")
 		default:
-			return err
+			return fmt.Errorf("readJSON: Got error %w", err)
 		}
 	}
 	return nil
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
